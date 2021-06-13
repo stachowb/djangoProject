913115164904346ccd3c5e99dbc32fcebeb6a00e
stachowb <stachow.bartlomiej@gmail.com>
2021-06-13 23:42:37 +0200

    initial

--- apps/prospects/views.py
+++ apps/prospects/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from django.contrib.auth.forms import UserCreationForm
 
 
 class HomeView(TemplateView):
     template_name = "base.html"
+
+
+class UserCreationView(FormView):
+    form_class = UserCreationForm
+    template_name = "register.html"
