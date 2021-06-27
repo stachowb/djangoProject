d94617a18efbfd9ba55363b7426700cdd944777c
stachowb <stachow.bartlomiej@gmail.com>
2021-06-28 01:57:41 +0200

    initial

--- frontend_vue/src/App.vue
+++ frontend_vue/src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Django</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/test1" class="navbar-item">Test1</router-link>
+          <router-link to="/test2" class="navbar-item">Test2</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/login" class="button is-light">Login</router-link>
+              <router-link to="/register" class="button is-success">Register</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "../node_modules/bulma";
 </style>
