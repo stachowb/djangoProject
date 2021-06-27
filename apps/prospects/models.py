08854ab5a3495ccd900b089f1bd52878d62776bf
stachowb <stachow.bartlomiej@gmail.com>
2021-06-28 01:03:13 +0200

    initial

--- apps/prospects/models.py
+++ apps/prospects/models.py
@@ -15,10 +15,6 @@ class ProspectProfile(models.Model):
     cv_file = models.FileField()
     skills = models.ManyToManyField(Skills, through="ProspectSkillset")
 
-
-
-
-
 class ProspectSkillset(models.Model):
     user = models.ForeignKey(ProspectProfile, on_delete=models.CASCADE)
     skill = models.ForeignKey(Skills, on_delete=models.CASCADE)
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     ###
+    'rest_framework'
     'crispy_forms',
     ###
     'apps.prospects'
