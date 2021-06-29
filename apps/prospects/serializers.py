064bccce6c3e23364715b5644ee60c65f3b427b7
Bartek Stachow <stachow.bartlomiej@gmail.com>
2021-06-30 00:09:52 +0200

    1

--- apps/prospects/serializers.py
+++ apps/prospects/serializers.py
@@ -1,2 +1,10 @@
 from rest_framework.serializers import ModelSerializer
 
+from rest_framework.serializers import ModelSerializer
+from .models import Skills
+
+
+class SkillsSerializer(ModelSerializer):
+    class Meta:
+        model = Skills
+        fields = "__all__"
